c5cc933022761a90aff7bdf8050e15796c568eb4
mercy chelangat <chelangatmercy194@gmail.com>
2024-03-21 17:39:32 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Mercy';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+module.exports = { currentUser, welcomeMessage, excitedWelcomeMessage, shortGreeting };
